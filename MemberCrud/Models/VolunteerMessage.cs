c9d52d379d4cf42a60b064d7f0fdfe6921ead7d9
Offranel <francis.sengele@gmail.com>
2026-07-25 15:00:38 -0700

    Added VolunteerMessage model to store messages sent by volunteers to the church admin.

--- MemberCrud/Models/VolunteerMessage.cs
+++ MemberCrud/Models/VolunteerMessage.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Policy;
 using System.Text;
 
 namespace MemberCrud.NewFolder
 {
-    internal class VolunteerMessage
+    /// <summary>
+    /// To have a message from a volunteer to the church admin.
+    /// </summary>
+    public class VolunteerMessage
     {
+        public int Id { get; set; }
+
+        public int MemberId { get; set; }
+
+        public required string MessageText { get; set; }
+
+        public DateTime SentAt { get; set; }
     }
 }
