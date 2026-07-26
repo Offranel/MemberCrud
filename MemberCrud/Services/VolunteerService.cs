f8c3aa45b290ba80f33be7237b5b6646d532552d
Offranel <francis.sengele@gmail.com>
2026-07-25 21:00:39 -0700

    Refactor VolunteerService; add message methods and usings
    Refactored VolunteerService class formatting and added curly braces. Introduced SendVolunteerMessage (empty) and GetMessageHistoiry (returns empty list) methods. Added necessary using directives for models and collections.

--- MemberCrud/Services/VolunteerService.cs
+++ MemberCrud/Services/VolunteerService.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using MemberCrud.Models;
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace MemberCrud.Services;
 
-    public class VolunteerService
+public class VolunteerService
+{
+    public void SendVolunteerMessage(VolunteerMessage message) { }
+    
+        public List<VolunteerMessage> GetMessageHistoiry()
     {
-
+        return new List<VolunteerMessage>();
     }
+}
+
 
