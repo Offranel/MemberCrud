54fe6ce03f8c128dade8906a6da9bb19c645b570
Offranel <francis.sengele@gmail.com>
2026-07-25 20:59:39 -0700

    Refactor MemberService: add in-memory CRUD operations
    Refactored MemberService to remove duplicate usings and encapsulate a private List<Member> for in-memory storage. Added GetAllMembers, AddMember, and DeleteMember methods to provide basic CRUD functionality. Improved class structure and encapsulation.

--- MemberCrud/Services/MemberService.cs
+++ MemberCrud/Services/MemberService.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using MemberCrud.Models;
 
 namespace MemberCrud.Services;
 
-    public class MemberService
-    {
+public class MemberService  
+{
+    private List<Member> members = new List<Member>();
 
+    public List<Member> GetAllMembers()
+    {
+        return members;
     }
+    public void AddMember(Member member)
+    {
+        members.Add(member);
+    }
+
+    public void DeleteMember( Member member)
+    {
+        members.Remove(member);
+    }
+
+}
 
