22dd41068915ed149e15f472dddc0e074a87186c
Offranel <francis.sengele@gmail.com>
2026-07-25 20:05:15 -0700

    Add initial service class stubs for member management
    Added empty MemberService, MinistryService, and VolunteerService classes in the MemberCrud.Services namespace with standard using directives. These classes will serve as the foundation for future service logic.

---
+++ MemberCrud/Services/MemberService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemberCrud.Services;
+
+    public class MemberService
+    {
+
+    }
+
---
+++ MemberCrud/Services/MinistryService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemberCrud.Services;
+
+    public class MinistryService
+    {
+
+    }
+
