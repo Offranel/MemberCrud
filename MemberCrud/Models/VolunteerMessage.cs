944b74b7cc4baf3e4aa4fdb5ee31f51618edc4ba
Offranel <francis.sengele@gmail.com>
2026-07-25 20:59:06 -0700

    Refactor VolunteerMessage class structure and docs
    Moved VolunteerMessage to top-level class in MemberCrud.Models namespace.

--- MemberCrud/Models/VolunteerMessage.cs
+++ MemberCrud/Models/VolunteerMessage.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.Security.Policy;
 using System.Text;
 
-namespace MemberCrud.NewFolder
+namespace MemberCrud.Models;
+
+/// <summary>
+/// To have a message from a volunteer to the church admin.
+/// </summary>
+public class VolunteerMessage
 {
-    /// <summary>
-    /// To have a message from a volunteer to the church admin.
-    /// </summary>
-    public class VolunteerMessage
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public int MemberId { get; set; }
+    public int MemberId { get; set; }
 
-        public required string MessageText { get; set; }
+    public required string MessageText { get; set; }
 
-        public DateTime SentAt { get; set; }
-    }
+    public DateTime SentAt { get; set; }
 }
