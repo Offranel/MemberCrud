082f4cbfd20220823522f33b563cbbf329447083
Offranel <francis.sengele@gmail.com>
2026-07-28 20:15:51 -0700

    Implement full CRUD for MinistryService with ADO.NET
    Updated MinistryService to support Add, Update, Delete, and GetAll operations for the Ministry entity using ADO.NET and SQL Server LocalDB. Added necessary using directives and connection string configuration.

--- MemberCrud/Services/MinistryService.cs
+++ MemberCrud/Services/MinistryService.cs
@@ -1,21 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Data.SqlClient;
 using MemberCrud.Models;
+using System;
+using System.Collections.Generic;
 
 namespace MemberCrud.Services;
 
 public class MinistryService
 {
-    public void addMinistry(Ministry ministry) { }
+    private readonly string connectionString =
+        "Server=(localdb)\\MSSQLLocalDB;Database=MemberCrud;Trusted_Connection=True;";
 
-    public void UpdateMinistry(Ministry ministry) { }
+    public void AddMinistry(Ministry ministry)
+    {
+        using SqlConnection connection = new(connectionString);
 
-    public void DeleteMinistry(int id) { }
+        connection.Open();
 
-    public List<Ministry> GetAllMinistries() 
-    
-    { 
-        return new List<Ministry>(); 
+        string sql = @"INSERT INTO Ministries
+                      (Name, Description)
+                      VALUES
+                      (@Name, @Description)";
+
+        using SqlCommand command = new(sql, connection);
+
+        command.Parameters.AddWithValue("@Name", ministry.Name);
+        command.Parameters.AddWithValue("@Description", ministry.Description);
+
+        command.ExecuteNonQuery();
+    }
+
+    public void UpdateMinistry(Ministry ministry)
+    {
+        using SqlConnection connection = new(connectionString);
+
+        connection.Open();
+
+        string sql = @"UPDATE Ministries
+                      SET Name = @Name,
+                          Description = @Description
+                      WHERE Id = @Id";
+
+        using SqlCommand command = new(sql, connection);
+
+        command.Parameters.AddWithValue("@Id", ministry.Id);
+        command.Parameters.AddWithValue("@Name", ministry.Name);
+        command.Parameters.AddWithValue("@Description", ministry.Description);
+
+        command.ExecuteNonQuery();
+    }
+
+    public void DeleteMinistry(int id)
+    {
+        using SqlConnection connection = new(connectionString);
+
+        connection.Open();
+
+        string sql = "DELETE FROM Ministries WHERE Id = @Id";
+
+        using SqlCommand command = new(sql, connection);
+
+        command.Parameters.AddWithValue("@Id", id);
+
+        command.ExecuteNonQuery();
+    }
+
+    public List<Ministry> GetAllMinistries()
+    {
+        List<Ministry> ministries = new();
+
+        using SqlConnection connection = new(connectionString);
+
+        connection.Open();
+
+        string sql = "SELECT * FROM Ministries";
+
+        using SqlCommand command = new(sql, connection);
+        using SqlDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Ministry ministry = new Ministry
+            {
+                Id = Convert.ToInt32(reader["Id"]),
+                Name = reader["Name"].ToString(),
+                Description = reader["Description"].ToString()
+            };
+
+            ministries.Add(ministry);
+        }
+
+        return ministries;
     }
 }
-
