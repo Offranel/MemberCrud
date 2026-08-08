1c992738000d5a058be6053925a4d60db05f14cf
Offranel <francis.sengele@gmail.com>
2026-08-07 18:36:22 -0700

    Add MemberCrudDbContext with entity DbSets
    Added MemberCrudDbContext in MemberCrud.Data namespace.
    Defined DbSet properties for Member, Ministry, MemberMinistry,
    and VolunteerMessage entities. Configured context to use a
    local SQL Server database with a specified connection string.

---
+++ MemberCrud/Data/MemberCrudDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MemberCrud.Models;
+
+namespace MemberCrud.Data
+{
+    public class MemberCrudDbContext : DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Ministry> Ministries { get; set; }
+        public DbSet<MemberMinistry> MemberMinistries { get; set; }
+        public DbSet<VolunteerMessage> VolunteerMessages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=MemberCrud;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
