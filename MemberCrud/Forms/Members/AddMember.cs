eecbecf90d7a0060c0fc895afbb74bb55def71e1
IvannaOtero <iotero1982@students.cptc.edu>
2026-08-16 15:39:00 -0700

    Fix typo in CreatedAt property; add Migrations folder
    Corrected property name to CreatedAt in AddMember.cs and Member.cs for consistency. Updated AddMember.cs usage. Added Migrations folder to MemberCrud.csproj for EF Core support.

--- MemberCrud/Forms/Members/AddMember.cs
+++ MemberCrud/Forms/Members/AddMember.cs
@@ -132,7 +132,7 @@ namespace MemberCrud
                 DateOfBirth =
                     DateOnly.FromDateTime(DateOfBirthDtp.Value),
 
-                CreateAt = DateTime.Now
+                CreatedAt = DateTime.Now
             };
 
             try
