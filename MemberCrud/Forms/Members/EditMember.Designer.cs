721240e20e6771036d2e6487bb5111fb94da9a24
Offranel <francis.sengele@gmail.com>
2026-08-16 16:47:41 -0700

    Revamp EditMember form UI layout and sizing
    Updated EditMember form with larger fonts, increased spacing, and repositioned controls for a modern look. Enlarged form size from 800x629 to 1360x1032. Added click event handlers for Save and Cancel buttons. Adjusted AutoScaleDimensions for new layout.

--- MemberCrud/Forms/Members/EditMember.Designer.cs
+++ MemberCrud/Forms/Members/EditMember.Designer.cs
@@ -56,197 +56,222 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(32, 9);
+            label1.Location = new Point(54, 15);
+            label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(106, 25);
+            label1.Size = new Size(175, 41);
             label1.TabIndex = 0;
             label1.Text = "First Name: ";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(448, 9);
+            label2.Location = new Point(762, 15);
+            label2.Margin = new Padding(5, 0, 5, 0);
             label2.Name = "label2";
-            label2.Size = new Size(99, 25);
+            label2.Size = new Size(164, 41);
             label2.TabIndex = 1;
             label2.Text = "Last Name:";
             // 
             // SaveMemberBtn
             // 
-            SaveMemberBtn.Location = new Point(177, 566);
+            SaveMemberBtn.Location = new Point(301, 928);
+            SaveMemberBtn.Margin = new Padding(5, 5, 5, 5);
             SaveMemberBtn.Name = "SaveMemberBtn";
-            SaveMemberBtn.Size = new Size(154, 34);
+            SaveMemberBtn.Size = new Size(262, 56);
             SaveMemberBtn.TabIndex = 2;
             SaveMemberBtn.Text = "Save Member";
             SaveMemberBtn.UseVisualStyleBackColor = true;
+            SaveMemberBtn.Click += SaveMemberBtn_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(32, 95);
+            label3.Location = new Point(54, 156);
+            label3.Margin = new Padding(5, 0, 5, 0);
             label3.Name = "label3";
-            label3.Size = new Size(136, 25);
+            label3.Size = new Size(227, 41);
             label3.TabIndex = 3;
             label3.Text = "Phone Number:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(448, 95);
+            label4.Location = new Point(762, 156);
+            label4.Margin = new Padding(5, 0, 5, 0);
             label4.Name = "label4";
-            label4.Size = new Size(58, 25);
+            label4.Size = new Size(95, 41);
             label4.TabIndex = 4;
             label4.Text = "Email:";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(147, 186);
+            label5.Location = new Point(250, 305);
+            label5.Margin = new Padding(5, 0, 5, 0);
             label5.Name = "label5";
-            label5.Size = new Size(169, 25);
+            label5.Size = new Size(281, 41);
             label5.TabIndex = 5;
             label5.Text = "Membership Status:";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(326, 260);
+            label6.Location = new Point(554, 426);
+            label6.Margin = new Padding(5, 0, 5, 0);
             label6.Name = "label6";
-            label6.Size = new Size(77, 25);
+            label6.Size = new Size(125, 41);
             label6.TabIndex = 6;
             label6.Text = "Address";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(32, 308);
+            label7.Location = new Point(54, 505);
+            label7.Margin = new Padding(5, 0, 5, 0);
             label7.Name = "label7";
-            label7.Size = new Size(61, 25);
+            label7.Size = new Size(102, 41);
             label7.TabIndex = 7;
             label7.Text = "Street:";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(448, 308);
+            label8.Location = new Point(762, 505);
+            label8.Margin = new Padding(5, 0, 5, 0);
             label8.Name = "label8";
-            label8.Size = new Size(42, 25);
+            label8.Size = new Size(69, 41);
             label8.TabIndex = 8;
             label8.Text = "City";
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(32, 405);
+            label9.Location = new Point(54, 664);
+            label9.Margin = new Padding(5, 0, 5, 0);
             label9.Name = "label9";
-            label9.Size = new Size(55, 25);
+            label9.Size = new Size(91, 41);
             label9.TabIndex = 9;
             label9.Text = "State:";
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(448, 405);
+            label10.Location = new Point(762, 664);
+            label10.Margin = new Padding(5, 0, 5, 0);
             label10.Name = "label10";
-            label10.Size = new Size(110, 25);
+            label10.Size = new Size(183, 41);
             label10.TabIndex = 10;
             label10.Text = "Postal Code:";
             // 
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(147, 509);
+            label11.Location = new Point(250, 835);
+            label11.Margin = new Padding(5, 0, 5, 0);
             label11.Name = "label11";
-            label11.Size = new Size(116, 25);
+            label11.Size = new Size(191, 41);
             label11.TabIndex = 11;
             label11.Text = "Date of Birth:";
             // 
             // CancelMemberBtn
             // 
-            CancelMemberBtn.Location = new Point(394, 566);
+            CancelMemberBtn.Location = new Point(670, 928);
+            CancelMemberBtn.Margin = new Padding(5, 5, 5, 5);
             CancelMemberBtn.Name = "CancelMemberBtn";
-            CancelMemberBtn.Size = new Size(112, 34);
+            CancelMemberBtn.Size = new Size(190, 56);
             CancelMemberBtn.TabIndex = 12;
             CancelMemberBtn.Text = "Cancel";
             CancelMemberBtn.UseVisualStyleBackColor = true;
+            CancelMemberBtn.Click += CancelMemberBtn_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(70, 37);
+            textBox1.Location = new Point(119, 61);
+            textBox1.Margin = new Padding(5, 5, 5, 5);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(219, 31);
+            textBox1.Size = new Size(370, 47);
             textBox1.TabIndex = 13;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(482, 37);
+            textBox2.Location = new Point(819, 61);
+            textBox2.Margin = new Padding(5, 5, 5, 5);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(198, 31);
+            textBox2.Size = new Size(334, 47);
             textBox2.TabIndex = 14;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(70, 124);
+            textBox3.Location = new Point(119, 203);
+            textBox3.Margin = new Padding(5, 5, 5, 5);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(219, 31);
+            textBox3.Size = new Size(370, 47);
             textBox3.TabIndex = 15;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(482, 124);
+            textBox4.Location = new Point(819, 203);
+            textBox4.Margin = new Padding(5, 5, 5, 5);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(247, 31);
+            textBox4.Size = new Size(417, 47);
             textBox4.TabIndex = 16;
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(424, 186);
+            comboBox1.Location = new Point(721, 305);
+            comboBox1.Margin = new Padding(5, 5, 5, 5);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(182, 33);
+            comboBox1.Size = new Size(307, 49);
             comboBox1.TabIndex = 17;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(70, 336);
+            textBox5.Location = new Point(119, 551);
+            textBox5.Margin = new Padding(5, 5, 5, 5);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(219, 31);
+            textBox5.Size = new Size(370, 47);
             textBox5.TabIndex = 18;
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(482, 336);
+            textBox6.Location = new Point(819, 551);
+            textBox6.Margin = new Padding(5, 5, 5, 5);
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(198, 31);
+            textBox6.Size = new Size(334, 47);
             textBox6.TabIndex = 19;
             // 
             // textBox7
             // 
-            textBox7.Location = new Point(482, 433);
+            textBox7.Location = new Point(819, 710);
+            textBox7.Margin = new Padding(5, 5, 5, 5);
             textBox7.Name = "textBox7";
-            textBox7.Size = new Size(198, 31);
+            textBox7.Size = new Size(334, 47);
             textBox7.TabIndex = 20;
             // 
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(70, 433);
+            comboBox2.Location = new Point(119, 710);
+            comboBox2.Margin = new Padding(5, 5, 5, 5);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(182, 33);
+            comboBox2.Size = new Size(307, 49);
             comboBox2.TabIndex = 21;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(368, 504);
+            dateTimePicker1.Location = new Point(626, 827);
+            dateTimePicker1.Margin = new Padding(5, 5, 5, 5);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(300, 31);
+            dateTimePicker1.Size = new Size(507, 47);
             dateTimePicker1.TabIndex = 22;
             // 
             // EditMember
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 629);
+            ClientSize = new Size(1360, 1032);
             Controls.Add(dateTimePicker1);
             Controls.Add(comboBox2);
             Controls.Add(textBox7);
@@ -270,6 +295,7 @@
             Controls.Add(SaveMemberBtn);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(5, 5, 5, 5);
             Name = "EditMember";
             Text = "EditMember";
             ResumeLayout(false);
