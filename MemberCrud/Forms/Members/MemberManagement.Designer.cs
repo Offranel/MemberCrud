3faf58f55aa36b608b733163e1f369c070300172
Offranel <francis.sengele@gmail.com>
2026-08-16 17:53:27 -0700

    Update MemberManagement form UI layout and styles
    - Moved and resized AllMembersLsbx; set ForeColor and Margin
    - Repositioned, resized Add/Edit/Delete buttons; added Margins
    - Increased form AutoScaleDimensions and ClientSize
    - Set form ForeColor and Margin

--- MemberCrud/Forms/Members/MemberManagement.Designer.cs
+++ MemberCrud/Forms/Members/MemberManagement.Designer.cs
@@ -36,48 +36,55 @@
             // 
             // AllMembersLsbx
             // 
+            AllMembersLsbx.ForeColor = SystemColors.MenuHighlight;
             AllMembersLsbx.FormattingEnabled = true;
-            AllMembersLsbx.Location = new Point(59, 43);
+            AllMembersLsbx.Location = new Point(100, 71);
+            AllMembersLsbx.Margin = new Padding(5);
             AllMembersLsbx.Name = "AllMembersLsbx";
-            AllMembersLsbx.Size = new Size(325, 329);
+            AllMembersLsbx.Size = new Size(550, 537);
             AllMembersLsbx.TabIndex = 0;
             // 
             // AddMemberBtn
             // 
-            AddMemberBtn.Location = new Point(439, 78);
+            AddMemberBtn.Location = new Point(746, 128);
+            AddMemberBtn.Margin = new Padding(5);
             AddMemberBtn.Name = "AddMemberBtn";
-            AddMemberBtn.Size = new Size(169, 48);
+            AddMemberBtn.Size = new Size(287, 79);
             AddMemberBtn.TabIndex = 1;
             AddMemberBtn.Text = "Add a member";
             AddMemberBtn.UseVisualStyleBackColor = true;
             // 
             // EditMemberBtn
             // 
-            EditMemberBtn.Location = new Point(439, 189);
+            EditMemberBtn.Location = new Point(746, 310);
+            EditMemberBtn.Margin = new Padding(5);
             EditMemberBtn.Name = "EditMemberBtn";
-            EditMemberBtn.Size = new Size(169, 48);
+            EditMemberBtn.Size = new Size(287, 79);
             EditMemberBtn.TabIndex = 2;
             EditMemberBtn.Text = "Edit a member";
             EditMemberBtn.UseVisualStyleBackColor = true;
             // 
             // DeleteMemberBtn
             // 
-            DeleteMemberBtn.Location = new Point(439, 301);
+            DeleteMemberBtn.Location = new Point(746, 494);
+            DeleteMemberBtn.Margin = new Padding(5);
             DeleteMemberBtn.Name = "DeleteMemberBtn";
-            DeleteMemberBtn.Size = new Size(169, 46);
+            DeleteMemberBtn.Size = new Size(287, 75);
             DeleteMemberBtn.TabIndex = 3;
             DeleteMemberBtn.Text = "Delete a member";
             DeleteMemberBtn.UseVisualStyleBackColor = true;
             // 
             // MemberManagement
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1360, 738);
             Controls.Add(DeleteMemberBtn);
             Controls.Add(EditMemberBtn);
             Controls.Add(AddMemberBtn);
             Controls.Add(AllMembersLsbx);
+            ForeColor = Color.FromArgb(192, 0, 0);
+            Margin = new Padding(5);
             Name = "MemberManagement";
             Text = "MemberManagement";
             ResumeLayout(false);
