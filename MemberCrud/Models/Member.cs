0b122e2c59581fcf26f65865f77922cd3786cb77
IvannaOtero <iotero1982@students.cptc.edu>
2026-08-19 14:40:32 -0700

    Rename CreateAt to CreatedAt in Member class
    Renamed the property CreateAt to CreatedAt in the Member class to correct the naming and improve code clarity.

--- MemberCrud/Models/Member.cs
+++ MemberCrud/Models/Member.cs
@@ -105,7 +105,7 @@ public class Member
     /// system. It can be used for reporting, history, and
     /// administrative purposes.
     /// </summary>
-    public DateTime CreateAt { get; set; }
+    public DateTime CreatedAt { get; set; }
 
     public override string ToString()
     {
